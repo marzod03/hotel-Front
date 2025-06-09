f2cb366577789a1c698254ec4d17636851efeb86
marzod03 <128107456+marzod03@users.noreply.github.com>
2025-06-08 22:05:27 -0600

    Update EditarHabitacion.jsx

--- src/pages/EditarHabitacion.jsx
+++ src/pages/EditarHabitacion.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Swal from "sweetalert2";
-import "../assets/ActualizarHabitacion.css";
+import "../assets/actualizarHabitacion.css";
 import.meta.env.VITE_API_URL;
 const API_URL = import.meta.env.VITE_API_URL;
 
